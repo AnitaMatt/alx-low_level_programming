495f05054c9ea3a5bc3ac6fd7789cef8506ab117
AnitaMatt <arneethar16@yahoo.com>
2022-03-11 03:13:57 -0800

    corrected my code to fit betty sytle

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,12 +2,13 @@
 /**
  * main - Entry point
  * Description: print the letters of the alphabet from a -z
- * Return 0
+ * Return: 0
  */
 int main(void)
 {
 	char letter;
-	for (letter = 'a';letter <= 'z';letter++)
+
+	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 	putchar('\n');
 	return (0);

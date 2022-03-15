d567d924c50949247d2c4dce55e15bc5eb459ba2
AnitaMatt <arneethar16@yahoo.com>
2022-03-15 05:44:03 -0700

    printing the alphabets from a function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * void print_alphabet(void) - Printing the alphabet from a function
+ * Return: 0
+ * Description: print
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}

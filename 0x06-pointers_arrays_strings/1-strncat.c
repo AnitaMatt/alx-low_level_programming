10babe3c2468f2d76440b0211d9ed4204226773d
AnitaMatt <arneethar16@yahoo.com>
2022-03-24 12:42:22 -0700

    1-strncat.c concatenating using bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings using bytes
+ * @dest:  destination string
+ * @src:   source string
+ * @n: number of bytes
+ * Return:  pointer to the destination string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+ 	int len = 0;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		len++;
+
+	for (i = 0; i < n && src[i]; i++)
+		dest[len + i] = src[i];
+
+	dest[len + i] = '\0';
+
+	return (dest);
+}

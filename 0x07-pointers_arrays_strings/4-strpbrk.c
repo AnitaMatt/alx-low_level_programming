17e62e0ac91aec14975b8d60dc876e932c1a783c
AnitaMatt <arneethar16@yahoo.com>
2022-03-28 15:07:06 -0700

    betty

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -14,7 +14,7 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0' && accept[i] != '\0'; i++)
 	{
-		if ((s[i] == accept[i]) || (s[i- 1] == accept[i]))
+		if ((s[i] == accept[i]) || (s[i - 1] == accept[i]))
 			return (s + i - 1);
 	}
 

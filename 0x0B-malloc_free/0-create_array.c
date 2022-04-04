0add0c2777c94664b16ff52a65f39980a2c00df9
AnitaMatt <arneethar16@yahoo.com>
2022-04-04 13:49:11 -0700

    return null if size is 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	s = malloc(sizeof(char) * size);
-	if (s == NULL)
+	if (s == NULL || size == 0)
 		return (NULL);
 
 	for (i = 0; i < size; i++)

ef15faf21f6de5c565f9af6fa87bddbabab315fb
AnitaMatt <arneethar16@yahoo.com>
2022-04-04 14:39:22 -0700

    adjusted betty styling

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,7 +17,7 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	for(i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 		len++;
 	s = malloc(sizeof(char) * len + 1);
 

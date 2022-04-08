8878ce43c19434d315a83e11c5d92228210988c9
AnitaMatt <arneethar16@yahoo.com>
2022-04-08 04:57:29 -0700

    corrected my size

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,13 +13,16 @@ int *array_range(int min, int max)
 {
 	int *new;
 	int i;
+	int size;
 
 	if (min > max)
 		return (NULL);
-	new = malloc(sizeof(int) * (max - min));
+	size = max - min + 1;
+
+	new = malloc(sizeof(int) * size);
 	if (new == NULL)
 		return (NULL);
-	for (i = 0; i < max; i++)
+	for (i = 0; i < size; i++)
 		new[i] = min++;
 	return (new);
 }

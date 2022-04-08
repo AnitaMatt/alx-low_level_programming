f38e588ce60a43d56fdf146079315e819b8c0772
AnitaMatt <arneethar16@yahoo.com>
2022-04-08 06:07:47 -0700

    bug fixes

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,7 +18,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 
 	if (new_size == old_size)
-		free(ptr);
+		return (ptr);
 	if ((ptr != NULL) && new_size == 0)
 	{
 		free(ptr);
@@ -26,7 +26,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	ptr_dup = ptr;
 	new = malloc(sizeof(*tmp) * (old_size + new_size));
-	if (new == NULL);
+	if (new == NULL)
 	{
 		free(ptr);
 		return (NULL);

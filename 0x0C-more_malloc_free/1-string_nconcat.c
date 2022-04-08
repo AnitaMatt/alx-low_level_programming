7f21c86f506ad413e17a6a28405d040020ecaa58
AnitaMatt <arneethar16@yahoo.com>
2022-04-08 03:24:01 -0700

    if fucntion fails return null

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,8 +29,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= lens2)
 		n = lens2;
 	new = malloc(sizeof(char) * (lens1 + n) + 1);
-
-
+	if (new == NULL)
+		return (NULL);
 	for (i = 0; i < lens1; i++)
 		new[i] = s1[i];
 	for (j = 0; j < n; j++)

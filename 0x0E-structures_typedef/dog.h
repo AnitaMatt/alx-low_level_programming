be0fac0769c85966eab95c0c10919348ba49c579
AnitaMatt <arneethar16@yahoo.com>
2022-04-11 12:38:07 -0700

    struct dog and its definition

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+/**
+ * struct dog - structure to define a dog
+ * @name: name member
+ * @age: age member
+ * @owner: owner member
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;

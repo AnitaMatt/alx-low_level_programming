6823c8ddf11f901240c8c0677624af56e0b7cb6e
Anita Matt <99275558+AnitaMatt@users.noreply.github.com>
2022-04-18 12:27:49 +0100

    Update variadic_functions.h
    And arranged it

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,11 +1,5 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
-
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-
-
 #include <stdarg.h>
 
 /**
@@ -19,6 +13,9 @@ typedef struct myPrintf
 	char * ch;
 	void (*f)(va_list args);
 } printAll;
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 

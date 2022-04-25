9392e513fff5cc97e0befa1e0619bfc4581e959a
AnitaMatt <arneethar16@yahoo.com>
2022-04-25 03:04:29 -0700

    printing bf main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@
 
 void first(void)
 {
-	printf("You're beat! and yet, you must allow,");
-	printf("\nI bore my house uponmy back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house uponmy back!\n");
 }

8c9d62d81a85cf15164b99238c0fc771b9a7a578
AnitaMatt <arneethar16@yahoo.com>
2022-04-28 09:04:45 -0700

    getting the value of abit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_bit - function that returns the value of a bit at
+ * a given index
+ * @n:  number
+ * index: index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int mask;
+	mask = n >> index;
+
+	return (mask & 1);
+}

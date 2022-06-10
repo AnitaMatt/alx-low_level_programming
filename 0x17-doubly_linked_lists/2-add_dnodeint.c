4e332a03fae1c8a99a6ac60ce5fca19f8c9270da
AnitaMatt <arneethar16@yahoo.com>
2022-06-10 04:21:32 -0700

    adding feature for empty list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,13 +15,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+
 	if (*head == NULL)
 		*head = new;
 	else
 	{
-		new->n = n;
 		new->next = *head;
-		new->prev = NULL;
 		(*head)->prev = new;
 		*head = new;
 	}
